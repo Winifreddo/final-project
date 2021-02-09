d6c88a83a237eb661061584dc5b76ccb4f316d75
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-02-09 12:34:24 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
      Weather
+<footer> Project completed by Leigh Richards and is {" "}
+     <a href="https://github.com/Winifreddo/final-project" target="_blank">
+        open-Sourced on GitHub
+     </a>
+    
+</footer>
     </div>
   );
 }
