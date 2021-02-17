1065f723ca590fb301e02d4c56c4876b0aa5e616
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-02-17 19:05:28 +0000

    replaced static icon URL with dynamic icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ function handleWeather (response) {
         wind: response.data.wind.speed,
         humidity: response.data.main.humidity,
         description: response.data.weather[0].description,
-        image: "https://ssl.gstatic.com/onebox/weather/64/snow.png",
+        image: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         date: new Date(response.data.dt*1000),
         city: response.data.name,
     });
