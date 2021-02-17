994b10dfe24aab06055dd5b62419b9c14377bea8
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-02-17 18:52:43 +0000

    set up search engine

--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,7 @@ export default function Weather (props) {
 
     let [weather, setWeather]=useState ({});
     let [loaded, setLoaded]=useState (false);
+    let [city, setCity]=useState(props.defaultCity);
 function handleWeather (response) {
     
     setWeather( {
@@ -17,16 +18,36 @@ function handleWeather (response) {
         description: response.data.weather[0].description,
         image: "https://ssl.gstatic.com/onebox/weather/64/snow.png",
         date: new Date(response.data.dt*1000),
+        city: response.data.name,
     });
 setLoaded (true);
 }
+
+function search () {
+    let apiKey="91a2969663631e6bc0bf8b6ad1300fee";
+let unit="metric";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+axios.get(apiUrl).then(handleWeather);
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    search(city);
+
+}
+
+function handleChange (event) {
+setCity(event.target.value);
+}
+
 if (loaded) {
     return (
         <div className="Weather"> 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="col-9">
-        <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" />
+        <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" onChange={handleChange} />
         </div>
         <div className="col-3">
         <input type="submit" value="search" className="btn btn-dark w-100" />
@@ -38,15 +59,7 @@ if (loaded) {
     );
 
 } else {
-
-
-
-let apiKey="91a2969663631e6bc0bf8b6ad1300fee";
-let unit="metric";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
-
-axios.get(apiUrl).then(handleWeather);
-
+    search();
 return "App currently loading...";
 }
     
